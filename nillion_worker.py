f4a6a28c5f20388e2df0d2abdcfdab26e4a99d8f
Memo Ugurbil <memo@nillion.com>
2025-01-28 16:28:45 -0600

    judge and generate key

--- nillion_worker.py
+++ nillion_worker.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from collections import defaultdict
 from dotenv import load_dotenv
 from game_sdk.game.worker import Worker
 from game_sdk.game.custom_types import (
@@ -16,6 +17,8 @@ import uuid
 
 import nilql
 
+from get_nildb_keys import get_nildb_keys
+
 load_dotenv()
 
 GAME_API_KEY = os.environ["GAME_API_KEY"]
@@ -28,7 +31,7 @@ Meter: Iambic tetrameter (four iambic feet per line).
 Rhyme Scheme: ABAB for each stanza.
 Content Requirements: Include vivid imagery of a natural process of change, such as leaves falling and regrowing, or a river carving a new path. Incorporate themes of hope and resilience.
 Tone: Reflective and uplifting, with a focus on the positive aspects of transformation.
-The poem should use evocative language and focus on painting a clear mental image for the reader. 
+The poem should use evocative language and focus on painting a clear mental image for the reader.
 """
 
 '''
@@ -38,9 +41,11 @@ args = parser.parse_args()
 value = args.prompt
 '''
 
-TASK = "generate a poem. Upload the generated poem to nildb"
+TASK = "generate a poem, upload poem, judge poems"
 
 JSON_TYPE = Dict[str, Union[str, int, float, bool, None, List, Dict]]
+
+get_nildb_keys()
 
 with open(os.environ.get("NILDB_CONFIG", ".nildb.config.json")) as fh:
     CONFIG = json.load(fh)
@@ -52,6 +57,50 @@ class NilDBAPI:
         self.secret_key = nilql.SecretKey.generate(
             {"nodes": [{}] * len(CONFIG["hosts"])}, {"store": True}
         )
+
+    def download_poems(self) -> Dict:
+        """Download all records in the specified node and schema."""
+        try:
+            shares = defaultdict(list)
+            teams = defaultdict(list)
+            for idx, node in enumerate(self.nodes):
+                headers = {
+                    "Authorization": f'Bearer {node["bearer"]}',
+                    "Content-Type": "application/json",
+                }
+
+                body = {"schema": CONFIG["schema_id"], "filter": {"contest": CONFIG["contest"]}}
+
+                response = requests.post(
+                    f"https://{node['url']}/api/v1/data/read",
+                    headers=headers,
+                    json=body,
+                )
+                assert (
+                    response.status_code == 200
+                ), ("upload failed: " + response.content)
+                data = response.json().get("data")
+                for i, d in enumerate(data):
+                    shares[i].append(d["text"]["$share"])
+                    teams[i].append(d["team"])
+            for i in range(len(teams)):
+                assert(teams[i][0] == teams[i][j] for j in range(1, len(teams[i])))
+                teams[i] = teams[i][0]
+            decrypted = []
+            for k in shares:
+                decrypted.append(nilql.decrypt(self.secret_key, shares[k]))
+            messages = {}
+            judged = {"blue": True, "purple": True, "red": True}
+            for team, message in zip(teams, decrypted):
+                if len(messages) == len(judged):
+                    break
+                if teams[team] in judged and judged[teams[team]]:
+                    messages[teams[team]] = message
+                    judged[teams[team]] = False
+            return messages
+        except Exception as e:
+            print(f"Error retrieving records in node: {str(e)}")
+            return {}
 
     def data_download(self) -> JSON_TYPE:
         """Download all records in the specified node and schema."""
@@ -151,12 +200,12 @@ def generate_content(plan: str, **kwargs) -> Tuple[FunctionResultStatus, str, di
     ]
 
     stream = client.chat.completions.create(
-        model="google/gemma-2-2b-it", 
-    	messages=messages, 
+        model="google/gemma-2-2b-it",
+    	messages=messages,
     	max_tokens=500,
     	stream=True
     )
-    
+
     result = ''.join([chunk.choices[0].delta.content for chunk in stream])
 
     if result:
@@ -185,6 +234,49 @@ def upload_to_nildb(content: str, **kwargs) -> Tuple[FunctionResultStatus, str, 
     return FunctionResultStatus.FAILED, "No object specified", {}
 
 
+def judge_poems(plan: str, **kwargs) -> Tuple[FunctionResultStatus, str, dict]:
+    """
+    Function to download content from nildb and judge the downloaded poems
+
+    Args:
+        plan: Notation of agent action
+        **kwargs: Additional arguments that might be passed
+    """
+    nildb = NilDBAPI()
+    my_id = str(uuid.uuid4())
+
+    download = nildb.download_poems()
+    if not download:
+        return FunctionResultStatus.FAILED, "Download failed", {}
+
+    client = InferenceClient(api_key=HUGGINGFACE_API_KEY)
+
+    messages = [
+        {
+            "role": "user",
+            "content": f"red poem: {download['red']}. blue poem: {download['blue']}. purple poem: {download['purple']}. Do you prefer red or blue or purple? Give a score for each."
+        }
+    ]
+
+    stream = client.chat.completions.create(
+        model="google/gemma-2-2b-it",
+    	messages=messages,
+    	max_tokens=500,
+    	stream=True
+    )
+
+    result = ''.join([chunk.choices[0].delta.content for chunk in stream])
+
+    if result:
+        return (
+            FunctionResultStatus.DONE,
+            f"Successfully judged the poems: {result}",
+            {},
+        )
+
+    return FunctionResultStatus.FAILED, "Could not judge the poems", {}
+
+
 # Action space with all executables
 action_space = [
     Function(
@@ -205,6 +297,15 @@ action_space = [
             )
         ],
         executable=upload_to_nildb,
+    ),
+    Function(
+        fn_name="judge_poems",
+        fn_description="Judge poems in nildb",
+        args=[
+            Argument(
+                name="plan", type="task", description="Prompt of reasoning")
+            ],
+        executable=judge_poems,
     ),
 ]
 
